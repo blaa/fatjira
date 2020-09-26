2056dafd9cd16f974503bb1a64dd960b6169f2f0
Tomasz bla Fortuna <tomasz.fortuna@exatel.pl>
2020-09-26 20:48:06 +0200

    Handle and name almost all special key combinations.
    Probably ncurses does it without a hassle... I guess? Naming: F1-F12, M +
    F1-F12, INS, DEL, UP, DOWN, RIGHT, LEFT, M-INS, M-DELETE, BACKSPACE, M-letter,
    M-S-letter, C-letter, TAB

--- yacui/console.py
+++ yacui/console.py
@@ -64,10 +64,6 @@ class Console:
         self.wnd_status = curses.newwin(lines_status, cols,
                                         lines_view + lines_discovery, 0)
 
-    def print(self, message, endl="\n"):
-        # TODO: Message at bottom with a minimal time and auto clearing.
-        self.stdscr.addstr(message + endl)
-
     def _decode_ctrl(self, key):
         """
         Convert control codes ("\x01" - "\x1A") into ascii representation C-x
@@ -80,27 +76,125 @@ class Console:
             # Not control code
             return key
         letter = code + ord('a') - 1
-        return "C-" + chr(letter)
+        return ("C-" + chr(letter)).replace("C-i", "TAB")
+
+    def _parse_meta_o(self):
+        "Parse escape codes starting with M-o"
+        try:
+            code = self._decode_ctrl(self.wnd_view.get_wch())
+        except curses.error:
+            return "M-S-o"
+        mapping = {
+            'P': 'F1',
+            'Q': 'F2',
+            'R': 'F3',
+            'S': 'F4',
+        }
+        key = mapping.get(code)
+        if not key:
+            raise Exception(f"Unable to parse escape code {code}")
+        return key
+
+    def _parse_escape_codes(self):
+        # Decode escape codes
+        # M-[ X ~ -> Some key ; ~ is optional.
+        # M-[ 3 ~ -> Delete
+        # 2 -> Insert
+        # 1 -> Home, 4 -> End
+        # 5 -> PageUp, 6 -> PageDown
+        # M-[ A -> Up; B -> Down; C -> Right; D -> Left
+        single_map = {
+            'A': 'UP',
+            'B': 'DOWN',
+            'C': 'RIGHT',
+            'D': 'LEFT',
+        }
+        read_code = ""
+        while True:
+            try:
+                key = self.wnd_view.get_wch()
+                if key in single_map:
+                    return single_map[key]
+                if key == "~":
+                    if not read_code:
+                        raise Exception("Invalid escape code")
+                    break
+                read_code += key
+            except curses.error:
+                if not read_code:
+                    return 'M-['
+                else:
+                    raise Exception(f"Invalid escape code M-[ {read_code}")
+
+        tilde_map = {
+            '1': 'HOME',
+            '2': 'INS',
+            '3': 'DELETE',
+            '4': 'END',
+            '5': 'PGUP',
+            '6': 'PGDOWN',
+            '15': 'F5',
+            '17': 'F6',
+            '18': 'F7',
+            '19': 'F8',
+            '20': 'F9',
+            '21': 'F10',
+            '23': 'F11',
+            '24': 'F12',
+        }
+        prefix = ""
+        if ";" in read_code:
+            read_code, mod = read_code.split(';')
+            mod_map = {
+                "3": "M-",
+                "5": "C-",
+            }
+            prefix = mod_map.get(mod, "")
+
+        key = tilde_map.get(read_code)
+        if key:
+            return prefix + key
+        raise Exception(f"Invalid escape code multi-code: {read_code}")
+
+    def _parse_meta_escape(self):
+        # Decode escape
+        try:
+            key = self._decode_ctrl(self.wnd_view.get_wch())
+            if len(key) == 1 and key[0].isupper():
+                key = "S-" + key.lower()
+            key = ('M-' + key).replace('M-C-', 'C-M-')
+        except curses.error:
+            # Just escape
+            return 'ESC'
+
+        if key == "M-O":
+            return self._parse_meta_o()
+
+        if key != 'M-[':
+            return key
+
+        return self._parse_escape_codes()
 
     def get_key(self):
-        """Read key with half-delay and decode Control and Meta keys"""
+        """
+        Read key with half-delay and decode special combinations like Control and
+        Meta keys.
+
+        FIXME: This will cause a small delay when typing escape or M-[. Could
+        be done better.
+        """
         ESC = '\x1b'
+
         try:
             key = self._decode_ctrl(self.wnd_view.get_wch())
         except curses.error:
             return None
 
-        if key != ESC:
+        if key == '\x7f':
+            return 'BACKSPACE'
+        elif key != ESC:
             return key
-
-        # Decode escape
-        try:
-            key = self._decode_ctrl(self.wnd_view.get_wch())
-            return ("M-" + key).replace('M-C-', 'C-M-')
-        except curses.error:
-            # Just escape
-            return "ESC"
-
+        return self._parse_meta_escape()
 
     def resize(self, a, b):
         "Handle SIGWINCH signal which happens on terminal resize"
