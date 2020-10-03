40e0179dc383e59872b731c000db747e043f2188
Tomasz bla Fortuna <tomasz.fortuna@exatel.pl>
2020-10-03 21:19:29 +0200

    Handle scrolling of rendered issue.

--- fatjira/views/issue.py
+++ fatjira/views/issue.py
@@ -1,6 +1,7 @@
 import _curses
 
 from fatjira.views import CommonView
+from yacui import Binding
 
 
 class IssueView(CommonView):
@@ -13,48 +14,60 @@ class IssueView(CommonView):
         self.key = key
         self.issue = self.app.jira.get_issue(key)
         self.scroll = 0
+        self.fits = True
 
     def redraw(self, wnd: _curses.window):
         "Refresh the view display"
         wnd.clear()
-        self.app.renderer.on_wnd(wnd, self.app.theme, 0, 0,
-                                 self.scroll,
-                                 "issue_view.j2",
-                                 key=self.issue.key,
-                                 issue=self.issue.fields)
+        self.fits = self.app.renderer.on_wnd(wnd, self.app.theme, 0, 0,
+                                             self.scroll,
+                                             "issue_view.j2",
+                                             key=self.issue.key,
+                                             issue=self.issue.fields)
 
     def on_enter(self):
         super().on_enter()
-        self.app.bindings.register("w", "Worklogs", self.menu_worklog)
-        self.app.bindings.register("t", "Transitions", self.menu_transitions)
 
-        self.app.bindings.register(["n", "DOWN"], "Scroll down",
-                                   self.action_scroll_down)
-        self.app.bindings.register(["p", "UP"], "Scroll up",
-                                   self.action_scroll_up)
-        self.app.bindings.register(["N"], "Scroll down x10",
-                                   self.action_scroll_down_fast)
-        self.app.bindings.register(["P"], "Scroll up x10",
-                                   self.action_scroll_up_fast)
+        def render_callback():
+            if not self.fits:
+                self.app.bindings.enable(["n", "N"])
+            else:
+                self.app.bindings.disable(["n", "N"])
+
+            if self.scroll:
+                self.app.bindings.enable(["p", "P"])
+            else:
+                self.app.bindings.disable(["p", "P"])
+
+        self.app.bindings.register_all([
+            Binding("w", "Worklogs", self.menu_worklog),
+            Binding("t", "Transitions", self.menu_transitions),
+            Binding(["n", "DOWN"], "Scroll down", self.action_scroll_down),
+            Binding(["N"], "Scroll down x10", self.action_scroll_down_fast),
+            Binding(["p", "UP"], "Scroll up", self.action_scroll_up),
+            Binding(["P"], "Scroll up x10", self.action_scroll_up_fast),
+        ], render_callback)
 
     def on_leave(self):
         super().on_leave()
 
     def menu_worklog(self):
-        self.app.bindings.push()
-        self.app.bindings.register("q", "Back to issue", self.menu_back)
-        self.app.bindings.register("a", "Add worklog", self.action_worklog_add)
-        self.app.bindings.register("d", "Delete worklog", None)
-        self.app.bindings.register("e", "Edit worklog", None)
+        self.app.bindings.register_all([
+            Binding("q", "Back to issue", self.menu_back),
+            Binding("a", "Add worklog", self.action_worklog_add),
+            Binding("d", "Delete worklog", None),
+            Binding("e", "Edit worklog", None),
+        ], push=True)
         self.app.display.redraw_view()
 
     def menu_transitions(self):
-        self.app.bindings.push()
-        self.app.bindings.register("q", "Back to issue", self.menu_back)
-        # TODO: Just an example; should be configurable; probably per project.
-        self.app.bindings.register("d", "Done", None)
-        self.app.bindings.register("t", "To Do", None)
-        self.app.bindings.register("p", "In progress", None)
+        self.app.bindings.register_all([
+            Binding("q", "Back to issue", self.menu_back),
+            # TODO: Just an example; should be configurable; probably per project.
+            Binding("d", "Done", None),
+            Binding("t", "To Do", None),
+            Binding("p", "In progress", None),
+        ], push=True)
         self.app.display.redraw_view()
 
     def action_worklog_add(self):
